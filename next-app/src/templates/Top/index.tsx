e19dfaf49aa66d57af411b6c341cd14704b5be45
FukeKazki <kazkichi0906@gmail.com>
2020-03-26 01:20:48 +0900

    Add: Topページの作成 (#2)

---
+++ next-app/src/templates/Top/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+
+const Top: React.FC = () => {
+    return (
+        <React.Fragment>
+            <Container maxWidth='lg' component='main'>
+
+            </Container>
+        </React.Fragment>
+    )
+}
+
+export default Top
