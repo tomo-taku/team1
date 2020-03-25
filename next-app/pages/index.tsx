f3bdc551e8dedf62c333d3326b974020904c1e71
FukeKazki <kazkichi0906@gmail.com>
2020-03-26 00:38:47 +0900

    Add: Next.jsの初期セットアップ (#2)

---
+++ next-app/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>換気アプリ</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+        <h1>換気しようね！</h1>
+    </main>
+  </div>
+)
+
+export default Home
