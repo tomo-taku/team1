f460ab5225c04878684266ef92e921d0c3fee0fe
FukeKazki <kazkichi0906@gmail.com>
2020-03-26 15:35:38 +0900

    Update: 現在の濃度を表示するパネルの表示 (#2)

--- next-app/src/templates/Top/index.tsx
+++ next-app/src/templates/Top/index.tsx
@@ -4,20 +4,28 @@ import styled from 'styled-components'
 import LineGraph from '../../organisms/LineGraph'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
+import ConditionPanel from '../../organisms/ConditionPanel'
 
 const Top: React.FC = () => {
     return (
         <React.Fragment>
             <Container maxWidth='lg' component='main'>
 
+                <InformationContainer>
+                    <ConditionPanel
+                        dustConcentration={400}
+                        co2Concentration={200}
+                    />
+                </InformationContainer>
+
                 <GraphBox>
                     <GraphTitle variant='subtitle1'>ダストセンサ</GraphTitle>
-                    <LineGraph />
+                    <LineGraph/>
                 </GraphBox>
 
                 <GraphBox>
                     <GraphTitle variant='subtitle1'>二酸化炭素センサ</GraphTitle>
-                    <LineGraph />
+                    <LineGraph/>
                 </GraphBox>
 
             </Container>
@@ -26,17 +34,22 @@ const Top: React.FC = () => {
 }
 
 const GraphTitle = styled(Typography)`
-  margin-bottom: 8px;
-  font-weight: bold;
+    margin-bottom: 8px;
+    font-weight: bold;
 `
 
 const GraphBox = styled(Box)`
-  background-color: #fff;
-  margin-top: 4em;
-  padding: 2em;
-  border: 2px solid #d4d4d4;
-  -webkit-border-radius: 16px;-moz-border-radius: 16px;border-radius: 16px;
-  -webkit-box-shadow: 0 0 4px rgba(0, 0, 0, .3);-moz-box-shadow: 0 0 4px rgba(0, 0, 0, .3);box-shadow: 0 0 4px rgba(0, 0, 0, .3);
+    background-color: #fff;
+    margin-top: 4em;
+    padding: 2em;
+    -webkit-border-radius: 16px;-moz-border-radius: 16px;border-radius: 16px;
+    -webkit-box-shadow: 0 0 4px rgba(0, 0, 0, .3);-moz-box-shadow: 0 0 4px rgba(0, 0, 0, .3);box-shadow: 0 0 4px rgba(0, 0, 0, .3);
+`
+
+const InformationContainer = styled(Box)`
+    margin-top: 4em;
+    display: flex;
+    justify-content: space-around;
 `
 
 export default Top
